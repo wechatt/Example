c2ab4d2f3306103f4249cd4a20897467fcd40757
yong <ty386127735@gmail.com>
2019-05-20 23:01:31 +0800

    添加MainActivity到activity包中

--- app/src/main/java/com/thundersoft/mi/example/activity/MainActivity.java
+++ app/src/main/java/com/thundersoft/mi/example/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.thundersoft.mi.example;
+package com.thundersoft.mi.example.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.thundersoft.mi.example.R;
 
 public class MainActivity extends AppCompatActivity {
 
